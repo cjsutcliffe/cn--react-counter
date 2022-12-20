4667388a2cc56bc1ea03016624fe3b608213271c
Chris Sutcliffe <cjsutcliffe@gmail.com>
2022-12-20 13:37:49 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+// function App() {
+//   const [counter, setCounter] = useState(0);
+//   const incrementCounter = () => setCounter(counter + 1);
+//   let decrementCounter = () => setCounter(counter - 1);
+//   // if(counter<=0) {
+//   //   decrementCounter = () => setCounter(1);
+//   // }
+//   return (
+//     <div className='container'> 
+//       <button id='countDown' onClickFunc={decrementCounter}>-</button>
+//       <h1>{counter}</h1>
+//       <button id='countUp' onClickFunc={incrementCounter}>+</button>
+//     </div>
+//   );
+// }
+
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const add = () => {
+    setCounter(count => count + 1)
+  }
+
+  const subtract = () => {
+    setCounter(count => count - 1)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="container">
+      <button className="button" onClick={subtract}>-</button>
+      <h1 className='counter'>{counter}</h1>
+      <button className="button" onClick={add}>+</button>
     </div>
   );
 }
